776584f0013e32792922cbf67e0889a9e6805e1e
Korede456 <koredeodeyemi456@gmail.com>
2023-12-17 02:42:21 +0100

    instagram clone

--- insta_clone/src/pages/components/SuggestedUsers/SuggestedUsers.jsx
+++ insta_clone/src/pages/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,10 +1,23 @@
-import { VStack } from "@chakra-ui/react"
-import { SuggestedHeader } from "./SuggestedHeader"
+import { Text, Flex, VStack } from "@chakra-ui/react";
+import { SuggestedHeader } from "./SuggestedHeader";
 
 export const SuggestedUsers = () => {
   return (
-   <VStack py={8} gap={4}>
-    <SuggestedHeader />
-   </VStack>
-  )
-}
+    <VStack py={8} gap={4}>
+      <SuggestedHeader />
+      <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+        <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+          Suggested for you
+        </Text>
+        <Text
+          fontSize={12}
+          fontWeight={"bold"}
+          _hover={{ color: "gray.500" }}
+          cursor={"pointer"}
+        >
+          See All
+        </Text>
+      </Flex>
+    </VStack>
+  );
+};
