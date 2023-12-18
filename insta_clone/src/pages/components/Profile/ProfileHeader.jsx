64090a03c604cb6f4e9f6b6ce0ab12eb69997120
Korede456 <koredeodeyemi456@gmail.com>
2023-12-18 16:42:38 +0100

    instagram clone

--- insta_clone/src/pages/components/Profile/ProfileHeader.jsx
+++ insta_clone/src/pages/components/Profile/ProfileHeader.jsx
@@ -1,16 +1,77 @@
-import { Avatar, AvatarGroup, Flex } from "@chakra-ui/react"
+import {
+  Avatar,
+  AvatarGroup,
+  Flex,
+  VStack,
+  Text,
+  Button,
+} from "@chakra-ui/react";
 
 export const ProfileHeader = () => {
   return (
-    <Flex gap={{base:4, sm:10}} py={10} dirextion={{base:"column", sm:"row"}} >
-        <AvatarGroup
-        size={{base:"xl", md:"2xl"}}
-        justifySelf={"center"}
+    <Flex
+      gap={{ base: 4, sm: 10 }}
+      py={10}
+      direction={{ base: "column", sm: "row" }}
+    >
+      <AvatarGroup
+        size={{ base: "xl", md: "2xl" }}
+        alignSelf={"center"}
         mx={"auto"}
+      >
+        <Avatar name="boi_thompson" src="/profilepic.png" alt="profile pic" />
+      </AvatarGroup>
+      <VStack alignItems={"start"} gap={2} mx={"auto"} flex={1}>
+        <Flex
+          gap={4}
+          direction={{ base: "column", sm: "row" }}
+          alignItems={"center"}
+          w={"full"}
         >
-            <Avatar name="boi_thompson" src="/profilepic.png" alt="profile pic" />
-
-        </AvatarGroup>
+          <Text fontSize={{ base: "sm", md: "lg" }}>boi_thompson</Text>
+          <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+            <Button
+              bg={"white"}
+              color={"black"}
+              _hover={{ bg: "whiteAlpha.800" }}
+              size={{ base: "xs", md: "sm" }}
+            >
+              Edit Profile
+            </Button>
+          </Flex>
+        </Flex>
+        <Flex alignItems={"center"} gap={{ base: 2, sm: 4 }}>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              4
+            </Text>
+            Posts
+          </Text>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              149
+            </Text>
+            Followers
+          </Text>
+          <Text fontSize={{ base: "xs", md: "sm" }}>
+            <Text as="span" fontWeight={"bold"} mr={1}>
+              175
+            </Text>
+            Following
+          </Text>
+        </Flex>
+        <Flex alignItems={"center"} gap={4}>
+          <Text fontSize={"sm"} fontWeight={"bold"}>
+            boi_thompson
+          </Text>
+        </Flex>
+        <Flex>
+          {" "}
+          <Text fontSize={"sm"}>
+            Tutorials that are meant to level up your skills as a programmer
+          </Text>
+        </Flex>
+      </VStack>
     </Flex>
-  )
-}
+  );
+};
